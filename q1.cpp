d15e96c563f9075644bf2b598e9e1ea7d38bb8e4
Hradini <31266306+Hradini@users.noreply.github.com>
2018-02-08 09:55:57 +0000

    Update q1.cpp

--- q1.cpp
+++ q1.cpp
@@ -1,64 +1,10 @@
-include <iostream>
-using namespace std;
-struct node{
-	int data;
-	node *temp;
-};
-class list{
-	node *head, *tail;
-	public:
-		//default constructor
-		list(){
-			head=NULL;
-			tail=NULL;
-		}
-		//function to insert new node
-		void insert(int value){
-			node *temp = new node;
-			temp->data= value; //assigns value to temp
-			temp->next=NULL;//points temp to null
-			if (head == NULL){
-				head = temp;
-				tail=temp;
-			}else{
-				tail->next=temp; //points last node to temp
-				tail=temp;//points tail to temp
-			}
-		}
-		//function to count items
-		int countItems(){
-			node *cur=new node;
-			int i=0;
-			cur =head;
-			while(cur!=NULL){//traverse the list till end 
-				cur=cur->next;
-				++i;//keeps track of the number of nodes
-			}
-			return i;
-		}
-		//function to display all items in the list
-		void display(){
-			node*cur= new node;
-			cur=head;
-			//traverses the linked list and preint every data from the node
-			while(cur!=NULL){
-				cout<< cur->data << "-> ";
-				cur=cur->next;
-			}
-				cout<< "NULL\n";
-			}
-		
-		
-};
 class bubble{
 	int arr[100];
 	void sorta(int arr[], int size);
-	void sortll(node *temp);	
+	void sortll(node *head);
+	void displayarr(int arr[]);
+	
 };
-
-
-
-
 
 void bubble::sorta (int arr[], int n){
 	int i, j;
@@ -71,4 +17,28 @@ void bubble::sorta (int arr[], int n){
 			}
 		}
 	}	
-}	
+} 
+
+void bubble::sortll(node*head){
+	node*temp;
+	int i, j,n;
+	n= countItems();
+	for (i = 0; i < n; ++i){
+		for (j = 0; j < n-i-1; ++j){
+			if (temp->data > (temp->next)->data){
+				temp->data = temp->data + (temp->next)->data;
+				(temp->next)->data = temp->data - (temp->next)->data;
+				temp->data = temp->data - (temp->next)->data;
+				temp=temp->next;
+			}
+		}
+	}	
+} 
+
+void bubble::displayarr(int arr[], int n){
+	int i;
+	for (i = 0; i < n; ++i){
+		cout<<arr[i];
+	}	
+} 
+
