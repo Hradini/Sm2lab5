99ec4ba1a055c32e6e833eed05b8a602454fb0f2
Hradini <31266306+Hradini@users.noreply.github.com>
2018-02-09 23:03:04 +0530

    Update q1.cpp

--- q1.cpp
+++ q1.cpp
@@ -8,10 +8,10 @@ class bubble{
 
 void bubble::sorta (int arr[], int n){
 	int i, j;
-	for (i = 0; i < n; ++i){
-		for (j = 0; j < n-i-1; ++j){
+	for (i = 0; i < n; ++i){ //traverses array multiple times
+		for (j = 0; j < n-i-1; ++j){ //traverses the array
 			if (arr[j] > arr[j+1]){
-				arr[j] = arr[j]+arr[j+1];
+				arr[j] = arr[j]+arr[j+1]; //swaps elements
 				arr[j+1] = arr[j]-arr[j + 1];
 				arr[j] = arr[j]-arr[j + 1];
 			}
@@ -23,9 +23,9 @@ void bubble::sortll(node*head){
 	node*temp;
 	int i, j,n;
 	n= countItems();
-	for (i = 0; i < n; ++i){
-		for (j = 0; j < n-i-1; ++j){
-			if (temp->data > (temp->next)->data){
+	for (i = 0; i < n; ++i){//traverses ll multiple tim
+		for (j = 0; j < n-i-1; ++j){//traverses ll
+			if (temp->data > (temp->next)->data){ //swaps elements
 				temp->data = temp->data + (temp->next)->data;
 				(temp->next)->data = temp->data - (temp->next)->data;
 				temp->data = temp->data - (temp->next)->data;
